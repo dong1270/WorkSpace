4aabf251449c0b691bcefda3ff770722c924f5a1
BlackMarine <wkdeo0000@gmail.com>
2021-08-20 17:57:30 +0900

    netlify로 배포하기 / 주소 :  workspacece.netlify.app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 상규사랑해
         </a>
       </header>
     </div>
